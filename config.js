6371b59e3a54b1b7d51dde786e56a1552e1f6570
Zenzz XD <henikpurwanti2605@gmail.com>
2025-08-17 11:20:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -24,52 +24,64 @@ const config = {
                 {
                     thumbnail: '/emailsender_img.png',
                     name: 'Zenzxz Email Sender',
-                    desc: 'A powerful AI assistant built to help with everyday tasks and answer questions efficiently.',
-                    link: 'https://mind.hydrooo.web.id'
+                    desc: 'Zenzxz Email Sender is a fast and efficient tool designed to simplify email communication and boost productivity.',
+                    link: 'https://email.zenzxz.my.id'
                 },
                 {
-                    thumbnail: '/rynnui_img.png',
-                    name: 'Rynn UI (Open Source)',
-                    desc: 'A simple and easy-to-use API documentation interface built with Express.js.',
-                    link: 'https://github.com/rynn-k/Rynn-UI'
+                    thumbnail: '/apizenz_img.png',
+                    name: 'Zenzxz APIs',
+                    desc: 'Zenzxz APIs a clean, customizable API documentation interface with GitHub integration and developer support.',
+                    link: 'https://api.zenzxz.my.id'
                 },
                 {
-                    thumbnail: '/nekorinn_img.png',
-                    name: 'Nekorinn API\'s',
-                    desc: 'A streamlined REST API service designed for developers who need quick access to multiple API functionalities in one place.',
-                    link: 'https://api.nekorinn.my.id'
+                    thumbnail: '/tts_img.png',
+                    name: 'Zenzxz TTS',
+                    desc: 'Zenzxz TTS is a high-quality text-to-speech service offering seamless conversion with over 700 diverse voices across multiple languages.',
+                    link: 'https://tts.zenzxz.my.id'
                 },
                 {
-                    thumbnail: '/nekonime_img.png',
-                    name: 'Nekonime',
-                    desc: 'An anime streaming platform with a minimalist, ad-free, and user-friendly interface for a seamless viewing experience.',
-                    link: 'https://nekonime.nekorinn.my.id'
+                    thumbnail: '/uploader_img.png',
+                    name: 'Zenzxz Uploader',
+                    desc: 'Zenzxz Uploader is a lightweight and efficient tool that lets you upload files up to 4.5 MB with instant results.',
+                    link: 'https://uploader.zenzxz.dpdns.org'
                 },
                 {
-                    thumbnail: '/mnmlistai_img.png',
-                    name: 'Mnmlist AI',
-                    desc: 'A minimalist AI tool designed for simplicity and clarity. Focus on what matters—no clutter, just intelligent solutions.',
-                    link: 'https://mnmlist.up.railway.app'
+                    thumbnail: '/chat_img.png',
+                    name: 'Zenzxz Chat AI',
+                    desc: 'Let’s chat with AI—fast, smart, and personalized. Zenzxz Chat AI gives you instant access to various AI models tailored to what you need. Be it writing, coding, creativity, or analysis—just pick a mode and go!',
+                    link: 'https://chat.zenzxz.my.id'
+                },
+                {
+                    thumbnail: '/upscale_img.png',
+                    name: 'Zenzxz Upscaler',
+                    desc: 'Zenzxz Upscaler enables fast and reliable image enhancement—simply upload your image, and the system delivers an upscaled version with improved clarity and detail.',
+                    link: 'https://upscaler.zenzxz.my.id'
+                },                
+                {
+                    thumbnail: '/short_img.png',
+                    name: 'Zenzxz Shorturl',
+                    desc: 'Zenzxz ShortURL is a sleek, free tool to quickly convert long URLs into compact, shareable links.',
+                    link: 'https://shorturl.zenzxz.my.id'
                 }
             ],
             links: [
                 {
                     name: 'GitHub',
                     is_verified: true,
-                    link: 'https://github.com/rynn-k'
+                    link: 'https://ZenzzXD'
                 },
                 {
                     name: 'Instagram',
                     is_verified: true,
-                    link: 'https://instagram.com/ran.dyyn'
+                    link: 'https://instagram.com/_zenzxz'
                 },
                 {
                     name: 'Telegram',
                     is_verified: true,
-                    link: 'https://t.me/rynnajahh'
+                    link: 'https://t.me/zenzzxz'
                 }
             ],
-            interests: ['Coding', 'Music', 'Gaming', 'Anime']
+            interests: ['Coding', 'Music', 'Gaming']
         }
     }
 };
